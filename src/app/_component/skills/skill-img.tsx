652e28ba64a0aff692e502b30ca2fe575b549c2a
artem-sokolovsky <artem.sokolovsky.888@gmail.com>
2024-08-03 02:05:49 -0700

    Fix: 2th feedback complete

--- src/app/_component/skills/skill-img.tsx
+++ src/app/_component/skills/skill-img.tsx
@@ -3,14 +3,25 @@ import Image from "next/image";
 
 const Img = ({ hover }: { hover: boolean }) => {
   return (
-    <div className="pr-[24px] flex">
-      <Image
-        src={hover ? "image/arrow.svg" : "image/arrow1.svg"}
-        alt="arrow"
-        width={25}
-        height={25}
-        priority
-      />
+    <div>
+      <div className="pr-[24px] sm:flex hidden ">
+        <Image
+          src={hover ? "image/arrow.svg" : "image/arrow1.svg"}
+          alt="arrow"
+          width={25}
+          height={25}
+          priority
+        />
+      </div>
+      <div className="pr-[24px] sm:hidden flex ">
+        <Image
+          src="image/arrow.svg"
+          alt="arrow"
+          width={25}
+          height={25}
+          priority
+        />
+      </div>
     </div>
   );
 };
